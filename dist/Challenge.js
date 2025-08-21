6198e8c2662187784a581f36c47c45ff909a558f
CarlosEGoulart <goulart193@gmail.com>
2025-08-21 00:39:30 -0300

    Upd ResProb_2025.2

--- dist/Challenge.js
+++ dist/Challenge.js
@@ -4,7 +4,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class Challenge {
     primeirosPrimos(n) {
         let primes = [];
+        let a = 25;
+        let isPrime = true;
+        //enquanto o tamanho do array for menor que n
+        while (primes.length < n) {
+            //verifica se o número é primo
+            if (a % 2 == 0 && a !== 2) {
+                isPrime = false;
+            }
+            for (let i = 3; i <= Math.sqrt(a); i++) {
+                if (a % i === 0) {
+                    isPrime = false;
+                }
+            }
+            //adiciona o número ao array
+            if (isPrime) {
+                primes.push(a);
+            }
+            a++;
+        }
         return primes;
     }
 }
 exports.default = Challenge;
+let tests = new Challenge();
+console.log("Primeiros Primos: ", tests.primeirosPrimos(25));
--- src/Challenge.ts
+++ src/Challenge.ts
@@ -3,15 +3,31 @@
 export default class Challenge {
     public primeirosPrimos(n: number): number[] {
         let primes = [];
-
+        let a = 25;
+        let isPrime = true;
+        
+        //enquanto o tamanho do array for menor que n
         while (primes.length < n) {
-            if()
+            //verifica se o número é primo
+            if (a % 2 == 0 && a !== 2) {
+                isPrime = false;
+            }
 
+            for (let i = 3; i <= Math.sqrt(a); i++) {
+                if (a % i === 0) {
+                    isPrime = false;
+                }
+            }
 
-
-
-
-
-
-
+            //adiciona o número ao array
+            if (isPrime) {
+                primes.push(a);
+            }
+            a++;
+        }
+        return primes;
+    }
 }
+
+let tests = new Challenge();
+console.log("Primeiros Primos: ", tests.primeirosPrimos(25));
