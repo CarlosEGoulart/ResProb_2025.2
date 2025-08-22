567ac4510b9bf6d5a0e3f2c8cf6740a8d30b9933
CarlosEGoulart <goulart193@gmail.com>
2025-08-22 18:12:51 -0300

    Upd WandinhaProblem

---
+++ src/WandinhaProblem.ts
@@ -0,0 +1,15 @@
+// Carlos Eduardo Goulart Oliveira e Nicolas Siqueira Moraes
+
+export default class WandinhaProblem{
+    public Wandinha(){
+
+
+
+
+    }
+        
+}
+
+let test = new WandinhaProblem();
+console.log("Wandinha Test: ", test.Wandinha());
+    
