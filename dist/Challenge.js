c3e7fa2a59749a2a9e5d1473cf1498b0af939553
NicoSikMoraes <nicolas.moraes.ns@gmail.com>
2025-08-22 11:18:48 -0300

    update primeiros primos

--- dist/Challenge.js
+++ dist/Challenge.js
@@ -1,28 +1,29 @@
 "use strict";
 // Carlos Eduardo Goulart Oliveira e Nicolas Siqueira Moraes
-Object.defineProperty(exports, "__esModule", { value: true });
-class Challenge {
-    primeirosPrimos(n) {
-        let primes = [2];
-        let a = 2;
+exports.__esModule = true;
+var Challenge = /** @class */ (function () {
+    function Challenge() {
+    }
+    Challenge.prototype.primeirosPrimos = function (n) {
+        var primes = [2];
+        var a = 3;
         while (primes.length < n) {
-            let isPrime = true;
-            if (a % 2 == 0) {
-                isPrime = false;
-            }
-            for (let i = 3; i <= Math.sqrt(a); i += 2) {
+            var isPrime = true;
+            for (var i = 3; i <= Math.sqrt(a); i += 2) {
                 if (a % i === 0) {
                     isPrime = false;
+                    break;
                 }
             }
             if (isPrime == true) {
                 primes.push(a);
             }
-            a++;
+            a += 2; // Testar somente os ímpares
         }
         return primes;
-    }
-}
-exports.default = Challenge;
-let tests = new Challenge();
-console.log("Primeiros Primos: ", tests.primeirosPrimos(25));
+    };
+    return Challenge;
+}());
+exports["default"] = Challenge;
+var tests = new Challenge();
+console.log('Primeiros Primos: ', tests.primeirosPrimos(25));
--- src/Challenge.ts
+++ src/Challenge.ts
@@ -1,32 +1,29 @@
 // Carlos Eduardo Goulart Oliveira e Nicolas Siqueira Moraes
 
 export default class Challenge {
-    public primeirosPrimos(n: number): number[] {
-        let primes = [2];
-        let a = 2;
+  public primeirosPrimos(n: number): number[] {
+    let primes = [2];
+    let a = 3;
 
-        while (primes.length < n) {
-            let isPrime = true;
+    while (primes.length < n) {
+      let isPrime = true;
 
-            if (a % 2 == 0) {
-                isPrime = false;
-            }
-
-            for (let i = 3; i <= Math.sqrt(a); i += 2) {
-                if (a % i === 0) {
-                    isPrime = false;
-                }
-            }
-
-            if (isPrime == true) {
-                primes.push(a);
-            }
-            
-            a++;
+      for (let i = 3; i <= Math.sqrt(a); i += 2) {
+        if (a % i === 0) {
+          isPrime = false;
+          break;
         }
-        return primes;
+      }
+
+      if (isPrime == true) {
+        primes.push(a);
+      }
+
+      a += 2; // Testar somente os ímpares
     }
+    return primes;
+  }
 }
 
 let tests = new Challenge();
-console.log("Primeiros Primos: ", tests.primeirosPrimos(25));
+console.log('Primeiros Primos: ', tests.primeirosPrimos(25));
